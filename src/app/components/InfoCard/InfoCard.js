a75da3e3ef3d5108280bc764ea330ecf93123dc6
casimuerto <ivacesize@gmail.com>
2022-05-23 11:43:30 -0500

    Adding card's internal info and styles

--- src/app/components/InfoCard/InfoCard.js
+++ src/app/components/InfoCard/InfoCard.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./InfoCardStyles.css";
 const InfoCard = (props) => {
-	return <div className="InfoCard appear">{props.number}</div>;
+	return (
+		<div className="InfoCard appear">
+			<div className="cardHeader">
+				<p className="cardNumber">{props.number}</p>
+				<img src={props.info.image} />
+			</div>
+			<div className="writtenInfo">
+				<p>Name: {props.info.name}</p>
+				<p>Status: {props.info.status}</p>
+				<p>Species: {props.info.species}</p>
+				<p>Type: {props.info.type}</p>
+				<p>Gender: {props.info.gender}</p>
+			</div>
+		</div>
+	);
 };
 
 export { InfoCard };
